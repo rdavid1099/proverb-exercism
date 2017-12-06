8e85e2c5fefe7aa099bcc0c06743aa877d38e887
Ryan Workman <rdavid1099@gmail.com>
2017-12-06 14:43:33 -0700

    Initial commit - creating prover Exercism for Java

---
+++ src/test/java/ProverbTest.java
@@ -0,0 +1,17 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProverbTest {
+
+    @Test
+    public void firstTest() {
+    }
+
+    @Ignore("Remove to run test")
+    @Test
+    public void ignoredTest() {
+    }
+}
